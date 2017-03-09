d4a91634bae54a436693afdffd3f484e165cdac6
jks0586 <jks0586@gmail.com>
2017-03-10 00:48:15 +0530

    all files

--- controller.js
+++ controller.js
@@ -1,9 +1,10 @@
-
+var path =require('path'); 
 module.exports = {
   configure: function(app) {  
 	// home function
 	 app.get('/',function(req,res){
-		res.send('Home');
+		//res.send('Home');
+		res.sendFile(path.join(__dirname, 'catalog/','views/','common/', 'header.html'));
 	});
 		
   }
